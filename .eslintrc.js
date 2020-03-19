f249317ef91af26f1b0fe700ed7d228b24b0e86a
Matheus Santos alves <matheussantosalves4@gmail.com>
2020-03-19 15:44:46 -0300

    Private Routes config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,8 @@ module.exports = {
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "import/no-unresolved": "off"
+    "import/no-unresolved": "off",
+    "react/jsx-props-no-spreading": "off"
   },
 
 };
